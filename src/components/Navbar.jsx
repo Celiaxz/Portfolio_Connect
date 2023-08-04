fdf0ee8d4fc53934c88fe3df09bed261fc4c9bf5
Gábor Farkas <farkas.gz@gmail.com>
2023-08-04 17:56:53 +0200

    fnished signup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+    return(
+        <div className="navbar">
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/signup'>Signup</NavLink>
+        </div>
+    )
+}
+
+export default Navbar;
