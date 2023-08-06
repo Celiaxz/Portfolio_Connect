8d4c57c887effb9a5a11f0a8c5efeb2af42f23ec
aymiwash_github <emi.luong@gmail.com>
2023-08-06 12:47:24 +0200

    conflicts resolved and login page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import HomePage from './components/HomePage'
-import Signup from './components/Signup'
+import HomePage from './pages/HomePage'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
 import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   )
