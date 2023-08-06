660540495163faf2c86e16cfb91d17c76cdb4bc0
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-08-07 00:55:24 +0200

    create project response payload

--- src/pages/CreateProject.jsx
+++ src/pages/CreateProject.jsx
@@ -1,22 +1,37 @@
+import axios from "axios";
 import React from "react";
 import { useState } from "react";
 
 function CreateProject() {
-  const [project, setProject] = useState();
-  const [title, setTitle] = useState();
-  const [description, setDescription] = useState();
-  const [technologies, setTechnologies] = useState();
-  const [repositoryLink, setRepositoryLink] = useState();
-  const [projectFolder, setProjectFolder] = useState();
-  const handleSubmit = (e) => {
-    e.preventDefaut();
-    onsumbit({
-      title,
-      description,
-      technologies,
-      repositoryLink,
-      projectFolder,
-    });
+  const [project, setProject] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [technologies, setTechnologies] = useState("");
+  const [repositoryLink, setRepositoryLink] = useState("");
+  const [projectFolder, setProjectFolder] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const payload = {
+        title,
+        description,
+        technologies,
+        repositoryLink,
+        projectFolder,
+      };
+      const payloadJson = JSON.stringify(payload);
+      console.log("payload: ", payloadJson);
+      const response = await fetch("http://localhost:5005/project/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: payloadJson,
+      });
+      console.log("this is my POST response: ", response);
+    } catch (error) {
+      console.log("error while creating project: ", error);
+    }
   };
 
   return (
