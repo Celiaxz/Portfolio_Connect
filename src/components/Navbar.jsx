104830848fffe5bb2f79c0faff173736aca6b510
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-08-07 23:12:23 +0200

    update search project link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
       <NavLink to="/">Home</NavLink>
       <NavLink to="/signup">Signup</NavLink>
       <NavLink to="/login">Login</NavLink>
-      <NavLink to="/projects">All projects</NavLink>
+      <NavLink to="/search-projects">All projects</NavLink>
     </div>
   );
 }
