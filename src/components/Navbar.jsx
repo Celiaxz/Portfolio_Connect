08202f58a64a95ac098d6aa6795b6c6a7dbfe7b0
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-08-07 17:32:54 +0200

    update navbar Userpage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,21 @@
-import { NavLink, useNavigate } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
 import { AuthContext } from "../contexts/Auth.context";
-
 function Navbar() {
-  const { isLoggedIn, user } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    console.log("logging out");
-    localStorage.removeItem("authToken");
-    navigate("/login");
-  };
-
-  return (
+  const { user, isLoggedIn, handleLogout } = useContext(AuthContext);
+  return isLoggedIn ? (
+    <div className="navbar">
+      <NavLink to="/create-project">Create Project</NavLink>
+      <NavLink to={`/user/${user._id}`}>Profile</NavLink>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  ) : (
     <div className="navbar">
       <NavLink to="/">Home</NavLink>
       <NavLink to="/signup">Signup</NavLink>
       <NavLink to="/login">Login</NavLink>
-      <NavLink to="/create-project">Create Project</NavLink>
       <NavLink to="/projects">All projects</NavLink>
-      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
-
 export default Navbar;
