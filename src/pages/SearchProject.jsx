65246f184b45aa54a65b9be7c8eb3ddf9faf8b7b
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-08-07 19:49:51 +0200

    filter projects

--- src/pages/SearchProject.jsx
+++ src/pages/SearchProject.jsx
@@ -1,39 +1,76 @@
 // SearchProjects.jsx
-import React, { useState } from "react";
-import AllProjects from "./AllProjects";
+import React, { useEffect, useState } from "react";
 
+// function AllProjects() {
+//   const [projects, setProjects] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchAllProjects() {
+//       try {
+//         const response = await fetch("http://localhost:5005/project/all");
+//         if (response.status === 200) {
+//           const parsed = await response.json();
+//           setProjects(parsed);
+//         } else {
+//           setProjects([]);
+//         }
+//       } catch (error) {
+//         console.log("Error while fetching all projects:", error);
+//         setProjects([]);
+//       }
+//     }
+
+//     fetchAllProjects();
+//   }, []);
+// }
 function SearchProjects() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
+  // const [searchTerm, setSearchTerm] = useState("");
+  const [filteredProjects, setFilteredProjects] = useState([]);
+  const [projects, setProjects] = useState([]);
 
-  const handleSearch = async () => {
-    try {
-      const response = await fetch(
-        `http://localhost:5005/project/search?term=${searchTerm}`
-      );
-      if (response.status === 200) {
-        const parsed = await response.json();
-        setSearchResults(parsed);
-      } else {
-        setSearchResults([]);
+  useEffect(() => {
+    async function fetchAllProjects() {
+      try {
+        const response = await fetch(
+          "http://localhost:5005/project/all/projects"
+        );
+        if (response.status === 200) {
+          const parsed = await response.json();
+          setProjects(parsed);
+          setFilteredProjects(parsed);
+        } else {
+          setProjects([]);
+        }
+      } catch (error) {
+        console.log("Error while fetching all projects:", error);
+        setProjects([]);
       }
-    } catch (error) {
-      console.log("Error while searching projects:", error);
-      setSearchResults([]);
+    }
+
+    fetchAllProjects();
+  }, []);
+
+  const handleSearch = async (evt) => {
+    const value = evt.target.value;
+    if (value !== undefined && value.length > 0) {
+      const filteredProj = [];
+      projects.forEach((project) => {
+        if (project.title.toLowerCase().includes(value.toLowerCase())) {
+          filteredProj.push(project);
+        }
+      });
+      setFilteredProjects(filteredProj);
+    } else {
+      setFilteredProjects(projects);
     }
   };
 
   return (
     <>
-      <AllProjects /> {/* Display all projects here */}
       <h1>Search Projects</h1>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <button onClick={handleSearch}>Search</button>
-      {searchResults.map((project) => (
+      <input type="text" onChange={handleSearch} />
+      {/* <button onClick={handleSearch}>Search</button> */}
+      {filteredProjects.map((project) => (
         <div key={project._id} className="project-details">
           <p>Title: {project.title}</p>
           <p>Technologies: {project.technologies}</p>
