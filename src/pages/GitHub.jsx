3da5c187fbb3abad876f60fa25100b88e8f8ffbc
Gábor Farkas <farkas.gz@gmail.com>
2023-08-10 12:53:24 +0200

    updated errorhandling on githubPage

--- src/pages/GitHub.jsx
+++ src/pages/GitHub.jsx
@@ -28,66 +28,73 @@ function GitHub() {
     fetchUser();
   }, [id]);
 
-  // Calculate the index range for projects to display on the current page
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const projectsToDisplay = projects?.slice(startIndex, endIndex) || [];
+  if (projects) {
+    if (!projects.message) {
+      
+        // Calculate the index range for projects to display on the current page
+        const startIndex = (currentPage - 1) * itemsPerPage;
+        const endIndex = startIndex + itemsPerPage;
+        const projectsToDisplay = projects?.slice(startIndex, endIndex) || [];
 
-  const avatar = projects?.[0].owner.avatar_url;
-  const isLoading = projects !== undefined;
+        const avatar = projects?.[0].owner.avatar_url;
+        const isLoading = projects !== undefined;
 
-  // Calculate totalPages based on the number of projects
-  const totalPages = Math.ceil((projects?.length || 0) / itemsPerPage);
+        // Calculate totalPages based on the number of projects
+        const totalPages = Math.ceil((projects?.length || 0) / itemsPerPage);
 
-  // Define click handlers for pagination
-  const handlePrevClick = () => {
-    setCurrentPage(currentPage - 1);
-  };
+        // Define click handlers for pagination
+        const handlePrevClick = () => {
+          setCurrentPage(currentPage - 1);
+        };
 
-  const handleNextClick = () => {
-    setCurrentPage(currentPage + 1);
-  };
+        const handleNextClick = () => {
+          setCurrentPage(currentPage + 1);
+        };
 
-  return (
-    <>
-      {isLoading ? (
-        <div className="other-Users-container">
-          {/* <h2 className="user-Projects-Title">My Projects</h2> */}
-          <img src={avatar} alt="" />
-          <Row gutter={16}>
-            {projectsToDisplay.map((project) => (
-              <Col xs={24} sm={12} md={8} lg={6} xl={6} key={project._id}>
-                <Card
-                  className="other-Users-card"
-                  title={
-                    <span className="other-Users-card-title">
-                      {project.name}
-                    </span>
-                  }
-                >
-                  <p>{project.language}</p>
-                  <Link
-                    className="others-nav"
-                    key={project.html_url}
-                    to={project.html_url}
-                  >
-                    GitHub Project
-                  </Link>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-
-          <Pagination
-            currentPage={currentPage}
-            totalPages={totalPages}
-            onPrevClick={handlePrevClick}
-            onNextClick={handleNextClick}
-          />
-        </div>
-      ) : null}
-    </>
-  );
+      return (
+        <>
+            <div className="other-Users-container">
+              {/* <h2 className="user-Projects-Title">My Projects</h2> */}
+              <img src={avatar} alt="" />
+              <Row gutter={16}>
+                {projectsToDisplay.map((project) => (
+                  <Col xs={24} sm={12} md={8} lg={6} xl={6} key={project._id}>
+                    <Card
+                      className="other-Users-card"
+                      title={
+                        <span className="other-Users-card-title">
+                          {project.name}
+                        </span>
+                      }
+                    >
+                      <p>{project.language}</p>
+                      <Link
+                        className="others-nav"
+                        key={project.html_url}
+                        to={project.html_url}
+                      >
+                        GitHub Project
+                      </Link>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+    
+              <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPrevClick={handlePrevClick}
+                onNextClick={handleNextClick}
+              />
+            </div>
+        </>
+      )
+    } else {
+      return <h2>{errorMessage}</h2>
+    }
+  } else {
+    return <h2>...Loading</h2>
+  }
 }
 
 export default GitHub;
